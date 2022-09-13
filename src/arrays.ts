1197c935ea7beca0f9595cfbee406f998506b34f
Shreeya <shreeya@udel.edu>
2022-09-13 14:43:53 -0400

    fixed another function

--- src/arrays.ts
+++ src/arrays.ts
@@ -71,7 +71,10 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    return colors.every(
+        (color: string): boolean =>
+            color === "red" || color === "blue" || color === "green"
+    );
 }
 
 /**
