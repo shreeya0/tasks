65245764e8da239a45cb90bbdf11807bdc25fc96
Shreeya <shreeya@udel.edu>
2022-10-02 13:24:08 -0400

    fixed two components

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,30 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    type QuestionType = "multiple_choice_question" | "short_answer_question";
+    const Question_Transitions: Record<QuestionType, QuestionType> = {
+        multiple_choice_question: "short_answer_question",
+        short_answer_question: "multiple_choice_question"
+    };
+    const [questiontype, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function changeQuestionType(): void {
+        const newQuestion = Question_Transitions[questiontype];
+        setQuestionType(newQuestion);
+    }
+    return (
+        <div>
+            <div>
+                <Button onClick={changeQuestionType}>Change Type</Button>
+            </div>
+            <div>
+                {questiontype === "short_answer_question" ? (
+                    <span>Short Answer</span>
+                ) : (
+                    <span>Multiple Choice</span>
+                )}
+            </div>
+        </div>
+    );
 }
