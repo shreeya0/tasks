f46c79954d9fbddadba673abe1b08245cd772f1b
Shreeya <shreeya@udel.edu>
2022-10-02 13:54:02 -0400

    fixed two components

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,29 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [progress, setProgress] = useState<boolean>(false);
+    const [attempt, setAttempt] = useState<number>(4);
+    function changeProgress(): void {
+        setProgress(progress === false ? true : false);
+        if (progress === false) {
+            setAttempt(attempt - 1);
+        }
+    }
+    return (
+        <div>
+            <span> attempts : {attempt}</span>
+            <Button
+                onClick={changeProgress}
+                disabled={progress || attempt === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={changeProgress} disabled={!progress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={() => setAttempt(1 + attempt)} disabled={progress}>
+                Mulligan
+            </Button>
+        </div>
+    );
 }
